2c1f50f2e7ea1d6d89e5cccb6102a6f6c2cabd43
zlatanajanovic <ajanovic.zlatan@gmail.com>
2017-07-15 18:50:54 +0200

    up

--- src/particle_filter.cpp
+++ src/particle_filter.cpp
@@ -19,6 +19,9 @@
 
 using namespace std;
 
+random_device seed_gen;
+default_random_engine engine(seed_gen());
+
 void ParticleFilter::init(double x, double y, double theta, double std[]) {
 	// Set the number of particles. Initialize all particles to first position (based on estimates of 
 	//   x, y, theta and their uncertainties from GPS) and all weights to 1. 
